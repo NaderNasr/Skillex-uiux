d595ef96bf342690635dd331fe2e6d653824e951
NaderNasr <nadernasr7@gmail.com>
2022-08-27 16:05:15 -0400

    search input

---
+++ src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Search.css'
+
+const Search = () => {
+  return (
+    <>
+      <input type="text" id="search" name="search" placeholder='Where do you want to go?'/>
+    </>
+  )
+}
+
+export default Search
