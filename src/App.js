b4baaccf98f14b7188fb4c738d8b02d05e95b295
NaderNasr <nadernasr7@gmail.com>
2022-08-26 22:38:03 -0400

    toggle onclick open and close window

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState } from 'react';
+import './App.css'
 
 const App = () => {
+
+  const [isActive, setIsActive] = useState(!false);
+  console.log('1',isActive)
+
+  const handleClick = (e) => {
+    console.log(e.target.id)
+    setIsActive(current => !current);
+    console.log('2',isActive)
+  };
+
   return (
-    <div>App</div>
+    <>
+      <div id='main'>
+        <section className={isActive ? 'active panel' : 'panel'} id='panel' onClick={handleClick}>
+          <h3 className="text">Mountains</h3>
+        </section>
+      </div>
+    </>
   )
 }
 
