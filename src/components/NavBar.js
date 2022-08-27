71641995a0cfd928a2fe55a42454d5519e58011c
NaderNasr <nadernasr7@gmail.com>
2022-08-27 11:30:59 -0400

    navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import './Nav.css'
+import { Container } from '@mui/system';
+
+const NavBar = () => {
+
+  return (
+    <AppBar position="sticky" id='navbar' elevation={0}>
+      <Container id='container'>
+        <h1 id='logo'>FlyX</h1>
+        <nav>
+          <ul className='nav-links'>
+            <li><a href='#'>Services</a></li>
+            <li><a href='#'>Projects</a></li>
+            <li><a href='#'>About</a></li>
+          </ul>
+        </nav>
+        <a className='cta' href='#'>
+          <button>Contact</button>
+        </a>
+      </Container>
+    </AppBar>
+  );
+};
+export default NavBar;
