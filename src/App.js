d9b28a566c49f637d2b82d7a97af36fc1ba7a0c3
NaderNasr <nadernasr7@gmail.com>
2022-08-27 13:50:37 -0400

    toggle multi panels

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import { useState } from 'react';
 import './App.css'
+import NavBar from './components/NavBar';
 
 const App = () => {
 
-  const [isActive, setIsActive] = useState(!false);
-  console.log('1',isActive)
+  const [panel, setPanel] = useState(true);
+  const [panel2, setPanel2] = useState(false);
+  const [panel3, setPanel3] = useState(false);
+  const [panel4, setPanel4] = useState(false);
 
-  const handleClick = (e) => {
-    console.log(e.target.id)
-    setIsActive(current => !current);
-    console.log('2',isActive)
-  };
+
+  const toggleReadMore = () => {
+    setPanel(!panel)
+    setPanel2(false)
+    setPanel3(false)
+    setPanel4(false)
+  }
+
+  const toggleReadMore2 = () => {
+    setPanel2(!panel2)
+    setPanel(false)
+    setPanel3(false)
+    setPanel4(false)
+
+  }
+
+  const toggleReadMore3 = () => {
+    setPanel3(!panel3)
+    setPanel2(false)
+    setPanel(false)
+    setPanel4(false)
+
+  }
+
+  const toggleReadMore4 = () => {
+    setPanel4(!panel4)
+    setPanel2(false)
+    setPanel(false)
+    setPanel3(false)
+
+  }
+
 
   return (
     <>
+      <NavBar />
       <div id='main'>
-        <section className={isActive ? 'active panel' : 'panel'} id='panel' onClick={handleClick}>
-          <h3 className="text">Mountains</h3>
-        </section>
+      <div className='hero-words'>
+        <h1>Travel.</h1>
+        <h1>Explore.</h1>
+        <h1>Learn.</h1>
+      </div>
+        <div className='panels'>
+          <section className={panel ? 'active panel' : 'panel'} id='panel' onClick={toggleReadMore}>
+            <h3 className="text">Restaurants</h3>
+          </section>
+          <section className={panel2 ? 'active panel' : 'panel'} id='panel' onClick={toggleReadMore2}>
+            <h3 className="text">Nature</h3>
+          </section>
+          <section className={panel3 ? 'active panel' : 'panel'} id='panel' onClick={toggleReadMore3}>
+            <h3 className="text">Wild Life</h3>
+          </section>
+          <section className={panel4 ? 'active panel' : 'panel'} id='panel' onClick={toggleReadMore4}>
+            <h3 className="text">Beach</h3>
+          </section>
+        </div>
       </div>
     </>
   )
