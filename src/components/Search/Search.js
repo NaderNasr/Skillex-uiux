fb1c5e750ac0e02210fa19201a5bc63f9aa8e2ff
NaderNasr <nadernasr7@gmail.com>
2022-08-30 15:37:16 -0400

    Google maps places autocomplete

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { SearchOutlined } from '@material-ui/icons'
+import { Autocomplete } from '@react-google-maps/api'
+import React, { useState } from 'react'
 import './Search.css'
 
-const Search = () => {
+const Search = ({ setCoords }) => {
+  const [autoComplete, setAutoComplete] = useState(null);
+
+  const onLoad = (word) => {
+    setAutoComplete(word)
+  }
+
+  const onPlaceChanged = () => {
+    const lat = autoComplete.getPlace().geometry.location.lat()
+    const lng = autoComplete.getPlace().geometry.location.lng()
+
+    setCoords({ lat, lng })
+  }
   return (
     <>
-      <input type="text" id="search" name="search" placeholder='Where do you want to go?'/>
+      <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged }>
+        <input type="text" id="search" name="search" placeholder='Where do you want to go?' />
+      </Autocomplete>
     </>
   )
 }
